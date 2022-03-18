9e505256a22fe8b4e577925458186b8d24170e6a
Chet Corcos <ccorcos@gmail.com>
2022-03-17 20:54:02 -0700

    Hmm. Can't reproduce the bug

--- renderer.js
+++ renderer.js
@@ -4,3 +4,23 @@
 // `nodeIntegration` is turned off. Use `preload.js` to
 // selectively enable features needed in the rendering
 // process.
+
+async function ping() {
+  const response = new Promise((resolve) => {
+    const handler = () => {
+      ipcRenderer.off("pong", handler);
+      resolve();
+    };
+    ipcRenderer.on("pong", handler);
+  });
+
+  ipcRenderer.send("ping");
+  await response;
+}
+
+window.demo = async () => {
+  for (let i = 0; i < 10000; i++) {
+    console.log("listenerCount", ipcRenderer.listenerCount("ping"));
+    await ping();
+  }
+};
